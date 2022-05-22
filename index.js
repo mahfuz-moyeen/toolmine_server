624ae91ec99b0646e5ee7371aed6f7d5eba3438f
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-05-22 14:13:41 +0600

    add mongodb

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4heos.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('mongo');
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('toolmine server on');
