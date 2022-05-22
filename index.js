2fba24ba3abbe7c03d0f3c975379cc3de2860d1c
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-05-22 20:48:23 +0600

    order delete api

--- index.js
+++ index.js
@@ -13,14 +13,29 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4heos.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// token function
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
+
 async function run() {
     try {
         await client.connect();
         const productsCollection = client.db("tool_mine").collection("products");
         const reviewsCollection = client.db("tool_mine").collection("reviews");
-        console.log('mongo');
         const userCollection = client.db("tool_mine").collection("users");
-        console.log('mongo');
+        const ordersCollection = client.db("tool_mine").collection("order");
 
         //---- login api ----//
 
@@ -34,7 +49,9 @@ async function run() {
                 $set: user
             }
             const result = await userCollection.updateOne(filter, updateDoc, options)
+
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+
             res.send({ result, token })
         })
 
@@ -57,6 +74,35 @@ async function run() {
             const query = { _id: ObjectId(id) }
             const product = await productsCollection.findOne(query)
             res.send(product);
+        })
+
+        // add orders 
+        app.post('/order', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.send(result)
+        })
+
+        // get orders  by user email
+        app.get('/orders', verifyToken, async (req, res) => {
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            if (email === decodedEmail) {
+                const query = { email: email }
+                const orders = await ordersCollection.find(query).toArray();
+                return res.send(orders);
+            }
+            else {
+                return res.status(403).send({ message: 'Forbidden access' })
+            }
+        })
+
+        //delete  order
+        app.delete('/order/:id', verifyToken, async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const result = await ordersCollection.deleteOne(filter)
+            res.send(result)
         })
 
         // add reviews 
